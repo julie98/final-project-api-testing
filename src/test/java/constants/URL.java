b97f1897a2eb2bd1ff092e268c972e5da5304652
Haoyu <hzhu64@ucsc.edu>
2023-07-19 15:39:27 -0400

    setting up project skeleton

---
+++ src/test/java/constants/URL.java
@@ -0,0 +1,18 @@
+package constants;
+
+import utils.ApplicationProperties;
+
+public enum URL {
+    USER("/user"),
+    TODOS("/todos");
+
+    private final String url;
+
+    URL(String path) {
+        this.url = ApplicationProperties.get("baseURI") + path;
+    }
+
+    public String toString() {
+        return url;
+    }
+}
