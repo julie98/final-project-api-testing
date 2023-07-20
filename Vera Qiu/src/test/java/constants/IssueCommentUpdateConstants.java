fce5919a56cfaa4cfff41e58b4f9882e44db42f8
Weirdguy790 <veralively7239@gmail.com>
2023-07-20 08:24:13 -0400

    Finished the issue management test.

---
+++ Vera Qiu/src/test/java/constants/IssueCommentUpdateConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class IssueCommentUpdateConstants {
+    public static final String COMMENT_PAYLOAD = "{\n" +
+            "  \"body\": \"This issue is easy\",\n" +
+            "  \"visibility\": {\n" +
+            "    \"type\": \"role\",\n" +
+            "    \"value\": \"developer\"\n" +
+            "  }\n" +
+            "}";
+}
